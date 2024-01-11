a5ea1ae523e434d53f44f8354f444ce789b094fa
John Flanagan <81453889+johnamii@users.noreply.github.com>
2024-01-10 23:34:34 -0800

    increment

---
+++ src/lib/Proomptr.svelte
@@ -0,0 +1,55 @@
+<script>
+    import { scale, fade, slide } from 'svelte/transition';
+
+    let state = 1;
+
+    let states = [
+        {type: "Chat Input", color: "bg-green-300", content: "What's the meaning of life?"},
+        {type: "Chat Output", color: "", content: "The meaning of life is a highly personalized and philosophical concept. It often relates to one's individual purpose, personal fulfillment, and the understanding of their own existence. It can be influenced by various factors including religious beliefs, relationships, personal achievements, or making a positive impact on the world."},
+        {type: "Draw Input", color: "bg-black", content: "shrek on honeymoon"},
+        {type: "Draw Output", color: '', content: 'src\media\shrek_on_honeymoon.png'},
+        {type: "Search Input", color: 'bg-blue-400', content: "proomptr documentation"},
+        {type: "Youtube Input", color: 'bg-red-400', content: "funny wooper"},
+    ]
+
+    function cycleState() {
+        state = (state + 1) % states.length;
+    }
+
+    setInterval(cycleState, 2000)
+</script>
+
+<div 
+  class='w-1/2 flex flex-col items-center justify-center bg-[rgba(255,255,255,0.9)] p-2 rounded-lg '
+
+>
+    {#if states[state].includes('Input')}
+        <div transition:slide={{duration: 300}} class='w-full h-full flex items-center justify-between gap-4 p-2'>
+            <div class='p-1 px-3 bg-green-300 text-white rounded-md'>
+                Chat
+            </div>
+            <div class='grow'>
+                What's the meaning of life?
+            </div>
+            <span class='material-symbols-outlined text-[rgba(0,0,0,0.5)]'>
+                settings
+            </span>
+        </div>
+        {:else}
+        <div transition:slide={{duration:500}} class='flex flex-col items-center justify-center gap-1 px-2'>
+            <span class='material-symbols-outlined text-xs'> expand_less </span>
+            <div class='bg-[rgba(0,0,0,0.05)] p-4  rounded-md text-neutral-700'>
+                {#if state == 1}
+                    <div class='px-2'>
+                    </div>
+                {:else}
+                    <div class='relative rounded-md'>
+                        <div class='absolute w-full text-neutral-100 text-center top-0 left-0 bg-[rgba(0,0,0,0.2)] p-1'>"shrek on honeymoon"</div>
+                        <img src='src\media\shrek_on_honeymoon.png' alt='Should be a pic here...'/>
+                    </div>
+                {/if}
+            </div>
+            <span class='material-symbols-outlined text-xs'> expand_more </span>
+        </div>
+    {/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Proomptr from "$lib/Proomptr.svelte";
+
+</script>
+
+<div class='w-screen h-screen flex flex-col bg-neutral-800 overflow-hidden'>
+    <div class='h-full w-full flex flex-col items-center justify-center gap-6'>
+        <h1 class='text-5xl text-white'>Proomptr</h1>
+        <div class='text-neutral-400 text-lg'>
+            "A lightweight desktop tool to proompt your AI overlords."
+        </div>  
+        <!-- Proomptr Copy -->
+        <Proomptr/>
+        
+        <!-- Download -->
+        <div class='flex flex-col gap-2 items-center justify-center bg-neutral-900 text-white w-1/3 rounded-lg p-4'>
+            <h1 class='text-center text-lg'>Downloads</h1>
+            <!-- <div class='bg-neutral-800 h-[1px] w-3/4'></div>
+
+            <div class='w-full p-4 flex flex-col items-start justify-center gap-2'>
+                <div> &bull; <a href='https://proomptr.org' class='text-blue-500 underline'> Windows x64 Installer </a></div>
+                <div> &bull; <a href='https://proomptr.org' class='text-blue-500 underline'> MacOS ARM64 Installer </a></div>
+            </div> -->
+        </div>
+    </div>
+    <div class='absolute bottom-0 w-full flex flex-col items-center justify-center text-white gap-2'>
+        Learn More
+        <span class='material-symbols-outlined shadow-lg'>
+            expand_more
+        </span>
+    </div>
+</div>
