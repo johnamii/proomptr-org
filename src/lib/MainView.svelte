e1c9ee2866fabc4f0039117626937ca3d2c093f9
John Flanagan <81453889+johnamii@users.noreply.github.com>
2024-01-12 15:29:51 -0800

    attempt fix

--- src/lib/MainView.svelte
+++ src/lib/MainView.svelte
@@ -2,6 +2,7 @@
     import Proomptr from "./Proomptr.svelte";
     import Popups from "./Popups.svelte";
     import { fly } from "svelte/transition";
+    import background from '$lib/assets/desktop.jpg';
 </script>
 
 <div transition:fly|local={{duration:500, y:-500}} class='h-full w-full pt-0 flex flex-col items-center justify-start pt-4 gap-6'>
@@ -11,7 +12,7 @@
     </div>  
     <!-- Proomptr Copy -->
     <div class=' h-3/5 w-full relative flex items-center justify-center'>
-        <img class='h-full w-3/5 absolute z-[1] rounded-lg' src='src\lib\assets\desktop.jpg' alt='desktop'/>
+        <img class='h-full w-3/5 absolute z-[1] rounded-lg' src={background} alt='desktop'/>
         <Proomptr/>
     </div>
     
