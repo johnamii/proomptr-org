8a7b2304d85e67a8f5d27115bcdb18a8639e1d2a
John Flanagan <81453889+johnamii@users.noreply.github.com>
2024-01-12 17:14:53 -0800

    swap to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,12 +12,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-
-		paths: {
-			base: '/proomptr-org',
-		},
-
-		outDir: 'build',
 	},
 };
 
