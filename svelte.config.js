052c64892a63a02dc99c263db6aa4a6f3826f666
John Flanagan <81453889+johnamii@users.noreply.github.com>
2024-01-12 16:15:54 -0800

    fix deployment

--- svelte.config.js
+++ svelte.config.js
@@ -12,11 +12,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		
-		outDir: 'build',
 
-		path: {
-			base: '/proomptr-org',
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
 		}
 	},
 };
